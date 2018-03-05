d591f936ac71e4e4705db120368714d8ab2dca80
Hung Dinh <playerhung@gmail.com>
2018-03-05 13:53:56 +0700

    Initial Commit

---
+++ unit1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  unit1
+//
+//  Created by Hung Dinh on 3/5/18.
+//  Copyright © 2018 Hung Dinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
