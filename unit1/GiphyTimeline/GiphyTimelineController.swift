6f1f7f2a7480d49263351b997e3b71495b58fa38
Hung Dinh <playerhung@gmail.com>
2018-03-26 17:44:50 +0700

    update loadmore refresh

--- unit1/GiphyTimeline/GiphyTimelineController.swift
+++ unit1/GiphyTimeline/GiphyTimelineController.swift
@@ -10,38 +10,51 @@ import GiphyCoreSDK
 
 class GiphyTimelineController: UIViewController, UITableViewDelegate,UITableViewDataSource {
     var data : [GiphyResponseModel] = []
-    let listGIF = ["tp3gmSQQpxEXe","wFMRxRPQlloSA","aIByFBfdrLAn6","qRSjC2vzplXfG","PUT0OeM1S8U4E","SOs17hZeAivjW","aPOSkITTpRWSY"]
+    var offset = 0
+    var isLoading = false
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return listGIF.count
+        return data.count
     }
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableTimeLineView.dequeueReusableCell(withIdentifier: "GiphyCell") as! GiphyTableViewCell
-        if let element = self.data.first(where: {$0.key == self.listGIF[indexPath.row]})
+        if let element = self.data.first(where: {$0.key == data[indexPath.row].key})
         {
             cell.data = element
             cell.imageGiphy.image = element.image
-        }else{
-            getData(cell: cell,index: indexPath.row)
         }
+//        }else{
+//            getData(cell: cell,index: indexPath.row)
+//        }
         cell.delegate = self
         return cell
     }
     
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let lastData = self.data.count - 1
+        if !isLoading && indexPath.row == lastData {
+            offset=offset+1
+            self.loadData()
+        }
+    }
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
        let giphydetailController = GiphyDetailViewController(nibName:"GiphyDetailViewController",bundle:nil)
-        if let dataToDetail = self.data.first(where: {$0.key == self.listGIF[indexPath.row]}){
+        if let dataToDetail = self.data.first(where: {$0.key == data[indexPath.row].key}){
             print("send \(indexPath.row)")
             giphydetailController.data = dataToDetail
             self.navigationController?.pushViewController(giphydetailController, animated: true)
         }
     }
+
     
     @IBOutlet weak var tableTimeLineView: UITableView!
     let client = GPHClient(apiKey: "1dMKOZZdiHLLuXyIcPXc1uglGqtrjuqZ")
     override func viewDidLoad() {
         super.viewDidLoad()
+        getData()
         tableTimeLineView.delegate = self
         tableTimeLineView.dataSource = self
         tableTimeLineView.estimatedRowHeight = 200
@@ -55,41 +68,79 @@ class GiphyTimelineController: UIViewController, UITableViewDelegate,UITableView
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    func getData(cell: GiphyTableViewCell,index:Int){
-        let op = client.gifByID(listGIF[index]) { (response, error) in
-            if let url = response?.data?.images?.fixedHeight?.gifUrl {
-                DispatchQueue.main.async {
-                    var height = 0.0
-                    if let h = response?.data?.images?.fixedHeight?.height{
-                        height = Double(h)
-                    }
-                    
-                    var width = 0.0
-                    if let w = response?.data?.images?.fixedHeight?.width{
-                        width = Double(w)
-                    }
-                   
-                    do
-                    {
-                            var imgUrl = UIImage.gifImageWithURL(url)
-                            print("load \(index)")
-                            let ratio = CGFloat(height/width)
-                            let scaledHeight = ratio * (cell.frame.size.width-20)
-                            let element = GiphyResponseModel(height: Int16(scaledHeight), width: Int16(width), key:self.listGIF[index], url: url, image: imgUrl!)
-                        self.data.append(element)
-                        self.saveDataToDataBase(element: element)
-                        cell.data = element
-                        cell.imageGiphy.image = element.image
-
-                    }
-                    catch
-                    {
-                        print("can't load image")
-                    }
+    func getData(){
+        let cell = GiphyTableViewCell()
+        let lista = client.search("cute girl", media: GPHMediaType.gif, offset: offset, limit: 7, rating: GPHRatingType.ratedPG13, lang: GPHLanguageType.english, pingbackUserId: "") { (response, error) in
+            if let listData = response?.data{
+            DispatchQueue.main.async {
+            for imageInfo in listData {
+                var height = 0.0
+                if let h = imageInfo.images?.fixedHeight?.height{
+                    height = Double(h)
                 }
+                
+                var width = 0.0
+                if let w = imageInfo.images?.fixedHeight?.width{
+                    width = Double(w)
+                }
+                
+                do
+                {
+                    let url = imageInfo.images?.fixedHeight?.gifUrl!
+                    
+                    var imgUrl = UIImage.gifImageWithURL(url!)
+                    print("load successful)")
+                    let ratio = CGFloat(height/width)
+                    let scaledHeight = ratio * (cell.frame.size.width-20)
+                    let element = GiphyResponseModel(height: Int16(scaledHeight), width: Int16(width), key:imageInfo.id, url: url!, image: imgUrl!)
+                    self.saveDataIfNonExist(element:element)
+                    self.saveDataToDataBase(element: element)
+//                    cell.data = element
+//                    cell.imageGiphy.image = element.image
+                    
+                    self.tableTimeLineView.reloadData()
+                    self.isLoading = false
+                }
+              }
+                
+            }
             }
         }
-    }
+        }
+//        let op = client.gifByID(listGIF[index]) { (response, error) in
+//            if let url = response?.data?.images?.fixedHeight?.gifUrl {
+//                DispatchQueue.main.async {
+//                    var height = 0.0
+//                    if let h = response?.data?.images?.fixedHeight?.height{
+//                        height = Double(h)
+//                    }
+//
+//                    var width = 0.0
+//                    if let w = response?.data?.images?.fixedHeight?.width{
+//                        width = Double(w)
+//                    }
+//
+//                    do
+//                    {
+//                            var imgUrl = UIImage.gifImageWithURL(url)
+//                            print("load \(index)")
+//                            let ratio = CGFloat(height/width)
+//                            let scaledHeight = ratio * (cell.frame.size.width-20)
+//                            let element = GiphyResponseModel(height: Int16(scaledHeight), width: Int16(width), key:self.listGIF[index], url: url, image: imgUrl!)
+//                        self.data.append(element)
+//                        self.saveDataToDataBase(element: element)
+//                        cell.data = element
+//                        cell.imageGiphy.image = element.image
+//
+//                    }
+//                    catch
+//                    {
+//                        print("can't load image")
+//                    }
+//                }
+//            }
+//        }
+//    }
     
     func saveDataToDataBase(element:GiphyResponseModel){
         if let listGetGif:[Gif] = Gif.getGif(key: element.key!){
@@ -100,6 +151,14 @@ class GiphyTimelineController: UIViewController, UITableViewDelegate,UITableView
         print("\(Gif.getAllGif().count)")
     }
     
+    func saveDataIfNonExist(element:GiphyResponseModel){
+        if self.data.first(where: {$0.key == element.key}) == nil
+        {
+            self.data.append(element)
+            print("save \(element.key)")
+        }
+    }
+    
     lazy var refreshController: UIRefreshControl = {
         let refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
@@ -108,10 +167,20 @@ class GiphyTimelineController: UIViewController, UITableViewDelegate,UITableView
     
     @objc private func refresh() {
         data = []
-        tableTimeLineView.reloadData()
+        getData()
         refreshController.endRefreshing()
     }
+    
+    func loadData() {
+        isLoading = true
+        getData()
+    }
+
+    
+    
 }
+
+
 extension GiphyTimelineController:ClickComment{
     func acctionCommentClick(model:GiphyResponseModel) {
         if let dataToDetail = self.data.first(where: {$0.key == model.key}){
